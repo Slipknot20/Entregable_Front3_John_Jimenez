83192c11709ceffe8d1750049522a661239ec9f3
JohnJimenez <jonja-148@hotmail.com>
2022-09-01 09:21:19 -0500

    Agregando App y Estilos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Cabecera from './components/Cabecera'
+import Listado from './components/Listado';
+import { useState } from 'react';
 
-function App() {
+
+
+// El componente App es el padre de:
+// - Cabecera
+// - Listado
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
+// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+
+function App(props) {
+const [contador, setContador] = useState(0);
+
+const incrementar = () =>{
+    setContador( contador+1);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cabecera contador={contador}/>
+      <Listado numero={incrementar}/>
     </div>
   );
 }
